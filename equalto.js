208f39257ff5fc81d2d173119e81ea35bbcf9d13
saikumark <kolichelimisaikumar9@gmail.com>
2022-08-18 07:43:57 +0530

    equalto

---
+++ equalto.js
@@ -0,0 +1,6 @@
+const a = 2, b = 6, c = 'Hi';
+
+// equal to operator
+console.log(a == '2');     
+console.log(b == '6');  
+console.log(c == 'Hi');  
---
+++ lessthan.js
@@ -0,0 +1,10 @@
+const a = 3, b = 2;
+
+// less than operator
+console.log(a < 2); // false
+console.log(b < 3); // true
+
+
+
+var num=5, num= 2;
+console.log(a<b);
